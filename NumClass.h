2dde54e38138460df093775d35cbb648c1d1116a
RoeyB49 <93865371+RoeyB49@users.noreply.github.com>
2021-11-07 18:14:13 +0200

    Add files via upload

---
+++ NumClass.h
@@ -0,0 +1,12 @@
+#ifndef _NUMCLASS_H_
+#define _NUMCLASS_H_
+
+int ArmHelper(int n, int counter,int power,int original);
+int PalindromeHelper(int n);
+int isArmstrong(int n);
+int isPalindrome(int n);
+int isPrime(int n);
+int Factorial(int n);
+int isStrong(int n);
+
+#endif
